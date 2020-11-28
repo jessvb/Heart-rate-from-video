3bf77f78ef81024d4d917b6bd49a30963da5f263
Jessica Van Brummelen <jess.vanbrummelen@gmail.com>
2020-11-27 16:00:35 -0800

    Analyze all files in recordings dir

--- README.md
+++ README.md
@@ -4,9 +4,8 @@
 ## Usage
 1. Clone the project locally
 2. In the project root, create a `recordings` folder and an `output` folder
-3. Place a `.avi` recording (that you'd like to use heart rate detection on) into the `recordings` directory
-4. Open the `heart-rate-post-process.py` file and change the `input_video` variable contents to be the name of your `.avi` file
-5. In a terminal, `cd` into the project root
-6. Run `pip install -r requirements.txt`
-7. Run `python heart-rate-post-process.py` to detect heart rate from your video
-8. View the output `.csv` file with the recognized heart rate in the `output` directory
+3. Place `.avi`/`.mp4` recording(s) (that you'd like to use heart rate detection on) into the `recordings` directory
+4. In a terminal, `cd` into the project root
+5. Run `pip install -r requirements.txt`
+6. Run `python heart-rate-post-process.py` to detect heart rate from your video(s)
+7. View the output `.csv` file with the recognized heart rate in the `output` directory
--- heart-rate-post-process.py
+++ heart-rate-post-process.py
@@ -8,59 +8,78 @@ from classes.video import Video
 import pandas as pd
 import os
 
-video = Video()
-process = Process()
-status = True
-frame = np.zeros((10,10,3),np.uint8)
-bpm = 0
 
 ################################################################################
-############ Change these depending on what you name the recordings ############
-input_video = 'video.avi'
-video.dirname = os.path.join('recordings',input_video)
-output_name = input_video.split('.')[0]
+######### Change these depending on where your recordings are located ##########
+rec_dir = 'recordings/'
 ################################################################################
-        
-# for exporting to csv
-bpm_all = []
-timestamps = []
 
-# Run the loop
-process.reset()
-video.start()
-max_frame_num = int(video.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-iter_percent = 0 # for printing percent done
-while status == True:
-    frame = video.get_frame()
 
-    if frame is not None:
-        process.frame_in = frame
-        process.run()
-            
-        f_fr = process.frame_ROI #get the face
-        bpm = process.bpm #get the bpm change over the time
-        
-        f_fr = cv2.cvtColor(f_fr, cv2.COLOR_RGB2BGR)
-        f_fr = np.transpose(f_fr,(0,1,2)).copy()
-
-        bpm_all.append(bpm)
-        curr_frame_num = video.cap.get(cv2.CAP_PROP_POS_FRAMES)
-        timestamps.append(curr_frame_num/video.fps)
-    else:
-        status = False
+def getVideoHeartRate(video,process,output_name):
+    frame = np.zeros((10,10,3),np.uint8)
+    bpm = 0
     
-    # every so often, show percent done
-    percent_done = curr_frame_num/max_frame_num*100
-    if (percent_done > iter_percent):
-        print('current frame: %.0f' % curr_frame_num)
-        print('percent done: %.1f%%' % percent_done)
-        iter_percent += 20
+    # for exporting to csv
+    bpm_all = []
+    timestamps = []
+
+    # Run the loop
+    process.reset()
+    video.start()
+    max_frame_num = int(video.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    iter_percent = 0 # for printing percent done
+    hasNextFrame = True
+    while hasNextFrame == True:
+        frame = video.get_frame()
+
+        if frame is not None:
+            process.frame_in = frame
+            process.run()
+
+            f_fr = process.frame_ROI #get the face
+            bpm = process.bpm #get the bpm change over the time
+
+            f_fr = cv2.cvtColor(f_fr, cv2.COLOR_RGB2BGR)
+            f_fr = np.transpose(f_fr,(0,1,2)).copy()
+
+            bpm_all.append(bpm)
+            curr_frame_num = video.cap.get(cv2.CAP_PROP_POS_FRAMES)
+            timestamps.append(curr_frame_num/video.fps)
+        else:
+            hasNextFrame = False
+        
+        # every so often, show percent done
+        percent_done = curr_frame_num/max_frame_num*100
+        if (percent_done > iter_percent):
+            print('current frame: %.0f' % curr_frame_num)
+            print('percent done: %.1f%%' % percent_done)
+            iter_percent += 20
 
 
-# Export predicted bpm to .csv format
-df = pd.DataFrame({'BPM': bpm_all, 'TIMESTAMP_SEC': timestamps})
-df.to_csv(os.path.join('output', output_name + '_video_bpm.csv'), sep=',', index=False)
+    # Export predicted bpm to .csv format
+    df = pd.DataFrame({'BPM': bpm_all, 'TIMESTAMP_SEC': timestamps})
+    df.to_csv(os.path.join('output', 'heartrate_' + output_name + '.csv'), sep=',', index=False)
 
-print('🎉 Done! 🎉')
-print('See the output file:')
-print('output/' + output_name + '_video_bpm.csv')
+    print('🎉 Done! 🎉')
+    print('See the output file:')
+    print('output/' + 'heartrate_' + output_name + '.csv')
+
+
+if __name__ == '__main__':
+    # Loop through specific files and analyze their video
+    files_in_dir = [f for f in os.listdir(rec_dir) if os.path.isfile(os.path.join(rec_dir, f))]
+    i = 0
+    for f in files_in_dir:
+        video = Video()
+        process = Process()
+        if f.split('.')[1] == 'avi' or f.split('.')[1] == 'mp4':
+            video.dirname = os.path.join(rec_dir,f)
+            output_name = f.split('.')[0]
+
+            print(f'Reading from {video.dirname}')
+
+            getVideoHeartRate(video, process, output_name)
+
+        i += 1
+        print(f"""Number of files to go: {len(files_in_dir) - i}
+            Percent files done: {i/len(files_in_dir)*100}\n""")
